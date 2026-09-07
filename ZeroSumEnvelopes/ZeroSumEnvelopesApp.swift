a74ebae09cbd873545b91dfa7878d664f1615fee
Noah Jett <noahjett@noahjett.com>
2026-09-07 17:00:02 -0500

    Initial Commit

---
+++ ZeroSumEnvelopes/ZeroSumEnvelopesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroSumEnvelopesApp.swift
+//  ZeroSumEnvelopes
+//
+//  Created by Noah Jett on 9/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ZeroSumEnvelopesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
